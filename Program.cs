2c4f138f28985d47a2b338570201e5c85a71ec61
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 10:28:58 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Total_Amount_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking input from the user
+            Console.WriteLine("Enter the deposit amount:");
+            double amount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Choose a savings option:");
+            Console.WriteLine("1: 5 years fixed deposit (10% interest rate)");
+            Console.WriteLine("2: Monthly interest (8.5% annual interest rate)");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    // Option 1: 5 years fixed deposit with 10% interest rate
+                    double interestRate1 = 0.10;
+                    double totalAmount = amount * Math.Pow(1 + interestRate1, 5);
+                    Console.WriteLine("Total amount after 5 years with 10% interest: {0:C}", totalAmount);
+                    break;
+
+                case 2:
+                    // Option 2: Monthly interest with 8.5% annual interest rate
+                    double annualInterestRate = 0.085;
+                    double monthlyInterestRate = annualInterestRate / 12; // Convert annual rate to monthly
+                    double monthlyInterestAmount = amount * monthlyInterestRate;
+                    Console.WriteLine("Monthly interest amount with 8.5% annual interest: {0:C}", monthlyInterestAmount);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option selected. Please choose 1 or 2.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Total Amount Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Total Amount Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc6f8f77-6e97-403d-b529-315c022938d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
